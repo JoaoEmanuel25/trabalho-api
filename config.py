1dd83375cbf6b51788e4314e75d63f0dd310e3e1
João Emanuel <ens-joaosilva@gmail.com>
2026-05-19 22:12:49 -0300

    chore: inclui config.py limpo para avaliacao e teste do professor

---
+++ config.py
@@ -0,0 +1,8 @@
+GEMINI_API_KEY = "bota a chave fi"
+OPENAI_API_KEY = "bota a chave fi"
+
+DB_HOST = "localhost"
+DB_NAME = "bancoapi"
+DB_USER = "postgres" 
+DB_PASSWORD = "postgres"
+DB_PORT = "5432"
